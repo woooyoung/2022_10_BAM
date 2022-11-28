0e2d533fa611c75838b41ee3a3de83573f4987a8
woooyoung <axdsw121@gmail.com>
2022-11-28 09:37:25 +0900

    게시물 삭제 구현중(id와 index가 일치하지 않는경우)

--- src/com/KoreaIT/java/BAM/Main.java
+++ src/com/KoreaIT/java/BAM/Main.java
@@ -53,12 +53,11 @@ public class Main {
 				}
 			} else if (command.startsWith("article detail ")) {
 				String[] commandDiv = command.split(" ");
-				int id = Integer.parseInt(commandDiv[2]); // "123" -> 123
+				int id = Integer.parseInt(commandDiv[2]);
 				Article foundArticle = null;
 				for (int i = 0; i < articles.size(); i++) {
 					Article article = articles.get(i);
-					// 0,1,2
-					// 1,2,3
+
 					if (article.id == id) {
 						foundArticle = article;
 						break;
@@ -73,6 +72,28 @@ public class Main {
 				System.out.printf("제목 : %s\n", foundArticle.title);
 				System.out.printf("내용 : %s\n", foundArticle.body);
 
+			} else if (command.startsWith("article delete ")) {
+				String[] commandDiv = command.split(" ");
+				int id = Integer.parseInt(commandDiv[2]);
+				Article foundArticle = null;
+				for (int i = 0; i < articles.size(); i++) {
+					Article article = articles.get(i);
+
+					if (article.id == id) {
+						foundArticle = article;
+						break;
+					}
+				}
+				if (foundArticle == null) {
+					System.out.printf("%d번 게시물은 존재하지 않습니다\n", id);
+					continue;
+				}
+				// 3개 ==> articles.size() == 3
+				// id    : 1 , 2 , 3
+				// index : 0 , 1 , 2
+				articles.remove(id - 1);
+				System.out.printf("%d번 게시물을 삭제했습니다\n", id);
+				
 			} else {
 				System.out.println("존재하지 않는 명령어입니다");
 			}
