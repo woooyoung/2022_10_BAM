8f0a698a040376ca10251304e330d920f296ae1a
woooyoung <axdsw121@gmail.com>
2022-11-30 11:48:12 +0900

    회원가입 기능 구현, 로그인 아이디 중복체크, 비밀번호 확인 절차 추가

---
+++ src/com/KoreaIT/java/BAM/dto/Member.java
@@ -0,0 +1,27 @@
+package com.KoreaIT.java.BAM.dto;
+
+public class Member {
+
+	public int id;
+	public String regDate;
+	public String updateDate;
+	public String loginId;
+	public String loginPw;
+	public String name;
+
+	public Member(int id, String regDate, String updateDate, String loginId, String loginPw, String name) {
+		this.id = id;
+		this.regDate = regDate;
+		this.updateDate = updateDate;
+		this.loginId = loginId;
+		this.loginPw = loginPw;
+		this.name = name;
+	}
+
+	@Override
+	public String toString() {
+		return "Member [id=" + id + ", regDate=" + regDate + ", updateDate=" + updateDate + ", loginId=" + loginId
+				+ ", loginPw=" + loginPw + ", name=" + name + "]";
+	}
+
+}
